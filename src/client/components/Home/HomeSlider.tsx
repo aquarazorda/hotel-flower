d42c0e70b898f1a77301059e179cb69e0a77d5d5
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-07-03 21:44:23 +0400

    Home improvements

--- src/client/components/Home/HomeSlider.tsx
+++ src/client/components/Home/HomeSlider.tsx
@@ -12,70 +12,81 @@ export const HomeSlider = () => {
   const { isDesktop } = useDevice();
   const navigate = useNavigate();
 
-  const [slider, { current, moveTo }] = createSlider(createMemo(() => ({
-    vertical: false,
-    mode: "snap",
-    rtl: false,
-    slides: {
-      perView: "auto",
-      spacing: isDesktop ? 110 : 9,
-    },
-  })));
+  const [slider, { current, moveTo }] = createSlider(
+    createMemo(() => ({
+      vertical: false,
+      mode: "snap",
+      rtl: false,
+      slides: {
+        perView: "auto",
+        spacing: isDesktop ? 110 : 9,
+      },
+    }))
+  );
 
   return (
     <div>
       <h5 class="mb-8 text-center text-sm  text-neutral-500">Rooms</h5>
       <div class="mb-14 cursor-pointer pl-4">
-      {/* @ts-ignore */}
-      <div use:slider>
-        <For each={data}>
-          {(room) => {
-            const url = getImageUrl(`/${room.id}/1`, isDesktop ? 1024 : 620);
+        {/* @ts-ignore */}
+        <div use:slider>
+          <For each={data}>
+            {(room) => {
+              const url = getImageUrl(`/${room.id}/1`, isDesktop ? 1024 : 620);
 
-            return (
-              <div class="text-sm text-white">
-                <div
-                  onClick={() => navigate("/rooms/" + room.id)}
-                  class="flex items-end rounded-lg bg-cover bg-center bg-no-repeat p-2
+              return (
+                <div class="text-sm text-white">
+                  <div
+                    onClick={() => navigate("/rooms/" + room.id)}
+                    class="flex items-end rounded-lg bg-cover bg-center bg-no-repeat p-2
                     xl:flex-col xl:items-start xl:px-12 xl:py-9 xl:font-semibold"
-                  style={{
-                    "background-image": `url(${url})`,
-                    "min-width": isDesktop ? "415px" : "122px",
-                    "max-width": isDesktop ? "415px" : "122px",
-                    height: isDesktop ? "434px" : "128px",
-                  }}
-                >
-                  <div class="hidden 2xl:block">
-                  <span class="xl:mt-auto xl:text-xl">
-                    {room.name}
-                  </span>
-                  <A href="/todo" class="mt-3 cursor-pointer rounded-lg bg-zinc-300/80 px-3 py-2 hover:bg-zinc-300/100">
-                      See More
-                    </A>
+                    style={{
+                      "background-image": `url(${url})`,
+                      "min-width": isDesktop ? "415px" : "122px",
+                      "max-width": isDesktop ? "415px" : "122px",
+                      height: isDesktop ? "434px" : "128px",
+                    }}
+                  >
+                    <div class="hidden 2xl:block">
+                      <span class="xl:mt-auto xl:text-xl">{room.name}</span>
+                      <A
+                        href="/todo"
+                        class="mt-3 cursor-pointer rounded-lg bg-zinc-300/80 px-3 py-2 hover:bg-zinc-300/100"
+                      >
+                        See More
+                      </A>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          }}
-        </For>
+              );
+            }}
+          </For>
+        </div>
+        <div class="mt-5 flex flex-col gap-4">
+          <div class="flex gap-3 xl:ml-8">
+            <Index each={data}>
+              {(room, idx) => (
+                <Show when={idx != data.length - 1}>
+                  <div
+                    class="h-2 w-2 cursor-pointer rounded-full hover:bg-neutral-400 xl:h-3 xl:w-3"
+                    onClick={() => moveTo(idx)}
+                    classList={{
+                      "bg-neutral-400": idx === current(),
+                      "bg-zinc-300": idx !== current(),
+                    }}
+                  />
+                </Show>
+              )}
+            </Index>
+          </div>
+          <div class="flex flex-col pr-3 xl:mx-auto xl:w-2/3 xl:gap-5">
+            <span class="text-xs text-neutral-500 xl:text-lg">
+              Experience Tbilisi's charm in Hotel Flower's rooms - a blend of
+              modern elegance, authentic Georgian charm, and comfort. Luxurious cityscapes await you.
+            </span>
+          </div>
+        </div>
       </div>
-      <div class="mt-6 flex gap-3 pl-2">
-        <Index each={data}>
-          {(room, idx) => (
-            <Show when={idx != data.length - 1}>
-              <div
-                class="h-2 w-2 cursor-pointer rounded-full hover:bg-neutral-400 xl:h-3 xl:w-3"
-                onClick={() => moveTo(idx)}
-                classList={{
-                  "bg-neutral-400": idx === current(),
-                  "bg-zinc-300": idx !== current(),
-                }}
-              />
-            </Show>
-          )}
-        </Index>
-      </div>
-    </div>
     </div>
   );
 };
--- src/routes/rooms/index.tsx
+++ src/routes/rooms/index.tsx
@@ -1,9 +1,10 @@
 import { For, Show } from "solid-js";
 import { roomsData } from "~/shared/data/rooms";
 import { getImageUrl } from "~/server/lib/cloudinary";
-import { useNavigate } from "solid-start";
+import { A, useNavigate } from "solid-start";
 
 import { SolarUserRoundedLinear } from "~/client/assets/icons/SolarRounded";
+import { Select } from '@kobalte/core';
 
 const Rooms = () => {
   const navigate = useNavigate();
@@ -14,32 +15,36 @@ const Rooms = () => {
   // });
   // return <DateRangePicker />`
   return (
-    <main class="flex flex-wrap justify-center gap-16 px-10 py-6">
+    <div>
+      {/* <div>
+        <Select.Root>
+          <Select.Label></Select.Label>
+        </Select.Root>
+      </div> */}
+      <main class="flex flex-wrap justify-center gap-10 px-11 py-6">
         <For each={roomsData}>
           {(room) => {
             const url = getImageUrl(`/${room.id}/1`, 1024);
 
             return (
-              <article class="flex flex-col" style={{ 'max-width': "439px" }}>
+              <article class="flex flex-col">
                 <div
                   style={{
-                    "max-width": "439px",
-                    height: "389px",
                     "background-image": `url(${url})`,
                   }}
-                  class="flex cursor-pointer flex-col gap-1 rounded-lg bg-cover bg-center p-6 font-medium text-white"
+                  onClick={() => navigate(`./${room.id}`)}
+                  class="flex h-64 cursor-pointer flex-col gap-1 rounded-lg bg-cover bg-center p-4 text-white"
                 >
-                  {/* <h2 class="mt-auto text-lg">{room.name}</h2>
                   <A
                     href={`./${room.id}`}
-                    class="mt-3 w-fit cursor-pointer rounded-lg bg-zinc-300/70 px-3 py-2 text-sm hover:bg-zinc-300"
+                    class="mt-auto w-fit cursor-pointer rounded-lg bg-neutral-600/80 p-2 text-[10px] font-medium hover:bg-zinc-300"
                   >
-                    See More
-                  </A> */}
+                    Check Price
+                  </A>
                 </div>
-                <div class="mt-4 flex flex-col gap-4">
-                  <h2 class="mt-auto text-lg font-semibold text-black/60">{room.name}</h2>
-                  <p class="w-11/12 text-base text-neutral-500">
+                <div class="mt-4 flex flex-col gap-4 text-xs text-neutral-500">
+                  <h2 class="mt-auto font-medium"><A href={`./${room.id}`}>{room.name}</A></h2>
+                  <p class="w-11/12">
                     Ideally located in Tbilisi City, Hotel offers a buffet
                     breakfast and free WiFi throughout the property. Each
                     accommodation at the 4-star hotel has city views, and guests
@@ -48,26 +53,27 @@ const Rooms = () => {
                     Tbilisi City, Hotel offers a buffet breakfast and free WiFi
                     throughout the property.
                   </p>
-                  <div class="flex gap-1">
+                  <div class="flex gap-1 text-black">
                     <For each={Array(room.persons)}>
-                      {() => <SolarUserRoundedLinear />}
+                      {() => <SolarUserRoundedLinear class="h-4 w-4" />}
                     </For>
                     <Show when={room.extraPerson}>
-                      + <SolarUserRoundedLinear />
+                      + <SolarUserRoundedLinear  class="h-4 w-4" />
                     </Show>
                   </div>
-                  <button 
+                  {/* <button 
                     class="mt-4 w-full rounded-md bg-secondary py-2 text-center font-medium uppercase text-white hover:bg-secondaryHover"
                     onClick={() => navigate(`./${room.id}`)}
                     >
                     Check Price
-                  </button>
+                  </button> */}
                 </div>
               </article>
             );
           }}
         </For>
       </main>
+    </div>
   );
 };
 
