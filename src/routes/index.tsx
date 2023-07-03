d096b7b4cc3d2b52dbcb1039f17d1e9c56de1ffd
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-07-03 20:26:26 +0400

    Text change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,8 @@
-import Footer from '~/client/components/Footer';
+import Footer from "~/client/components/Footer";
 import { MobileHeader } from "~/client/components/Header/mobile";
 import { HomeSlider } from "~/client/components/Home/HomeSlider";
 import { HomeMainBox } from "~/client/components/Home/MainBox";
-import { Restaurant } from '~/client/components/Home/Restaurant';
+import { Restaurant } from "~/client/components/Home/Restaurant";
 import { HomeSecondarySlider } from "~/client/components/Home/SecondarySlider";
 
 export default function Home() {
@@ -13,10 +13,9 @@ export default function Home() {
       <HomeMainBox />
       <div class="flex justify-center px-6 py-14 align-middle xl:m-16 xl:h-52 xl:p-16">
         <span class="inline-flex self-center text-center text-xs text-textSecondary xl:w-1/2 xl:text-lg">
-          Discover the vibrant spirit of Tbilisi, Georgia from the comfort of
-          Hotel Flower, a 4-star luxury hotel nestled in the very center of the
-          city. Hotel Flower is an exquisite retreat for travelers seeking the
-          perfect blend of modern elegance and authentic Georgian charm.
+          Ideally located in Tbilisi City. Each accommodation at the 4-star
+          hotel has city views, and guests can enjoy access to a shared lounge
+          and to a terrace.
         </span>
       </div>
       <HomeSlider />
