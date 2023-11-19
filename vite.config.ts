9c584a21073608aed6e4f83d1c201cef7ce47449
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-11-20 00:59:26 +0400

    package update test

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import solid from "solid-start/vite";
 import prpc from "@prpc/vite";
 import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
-import generateImageCountPlugin from './src/plugins/imageCount.plugin';
+import generateImageCountPlugin from "./src/plugins/imageCount.plugin";
 
 export default defineConfig({
   plugins: [
